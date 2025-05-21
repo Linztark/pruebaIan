bb05938b10a18c98348c036f1dffaf76b0147ba1
Linztark <anasilvesttttre@gmail.com>
2025-05-21 13:36:41 +0200

    Añadida edad

--- src/App.java
+++ src/App.java
@@ -3,5 +3,8 @@ public class App {
         System.out.println("Ian Palomares GALLEGO");
         System.out.println("Se había olvidado este chico de poner su segundo apellido");
         System.out.println("Ian Palomares es un vatito del fondo (come mucho)");
+        
+        int edadIan = 19;
+        System.err.println("Este mocoso tiene " + edadIan + " anos.");
     }
 }
