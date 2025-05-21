ee00b57b4dbbda864520027c9a64504ab7e13288
Linztark <anasilvesttttre@gmail.com>
2025-05-21 13:09:43 +0200

    He añadido el apellido que le faltaba

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ian Palomares");
+        System.out.println("Ian Palomares GALLEGO");
+        System.out.println("Se había olvidado este chico de poner su segundo apellido");
     }
 }
